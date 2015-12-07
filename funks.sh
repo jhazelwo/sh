f6d638c9ee7c8d15750ddd945187206ec100c8dd
John Hazelwood <jhazelwo@users.noreply.github.com>
2015-12-07 11:14:29 -0700

    Mon Dec  7 11:14:29 MST 2015

---
+++ funks.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Show arbit message like a logger would.
+yo(){ echo "`date` `hostname` $@";}
+
+
+# Wait $2 seconds for $1 to exist.
+wait_for_path() {
+    amount=$2
+    test -z "$amount" && amount=60
+    end="$(($(date '+%s')+${amount}))"
+    while [ ! -e $1 ]; do
+        test "`date '+%s'`" -gt "${end}" && { echo timeout; exit 9; }
+        sleep 1;
+    done
+}
+
+# Wait $2 seconds for $1 to exist as directory.
+wait_for_dir() {
+    amount=$2
+    test -z "$amount" && amount=60
+    end="$(($(date '+%s')+${amount}))"
+    while [ ! -d $1 ]; do
+        test "`date '+%s'`" -gt "${end}" && { echo timeout; exit 9; }
+        sleep 1;
+    done
+}
+
+# Wait $2 seconds for $1 to exist as file.
+wait_for_file() {
+    amount=$2
+    test -z "$amount" && amount=60
+    end="$(($(date '+%s')+${amount}))"
+    while [ ! -f $1 ]; do
+        test "`date '+%s'`" -gt "${end}" && { echo timeout; exit 9; }
+        sleep 1;
+    done
+}
+
+
