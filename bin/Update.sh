d6b49d2bdf2d4631f59edf4d087a5044be159a22
jhatdh <jhatdh@users.noreply.github.com>
2016-06-06 08:35:56 -0600

    made Update.sh also work with yum

--- bin/Update.sh
+++ bin/Update.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
-sudo /usr/bin/apt-get clean && \
-  sudo /usr/bin/apt-get update && \
-  sudo /usr/bin/apt-get upgrade -y
+# Blindly discard cache and upgrade this host.
+# For use on cattle only.
+#
+test -x /usr/bin/apt-get && {
+  sudo /usr/bin/apt-get clean && \
+    sudo /usr/bin/apt-get update && \
+    sudo /usr/bin/apt-get -y upgrade
+  exit $?
+}
+test -x /usr/bin/yum && {
+  sudo /usr/bin/yum -y clean all && \
+    sudo /usr/bin/yum -y upgrade
+  exit $?
+}
+exit 1
